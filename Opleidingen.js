fd4455185adab08e980cc11bf60c4d331e8da1d4
Kenny Lacroix <kenny.lacroix@telenet.be>
2021-06-13 23:46:52 +0200

    opleidingen object

---
+++ Opleidingen.js
@@ -0,0 +1,73 @@
+let Opleidingen = {
+  Elektromechanica: {
+    Opleiding: 'Elektromechanica',
+    Afstudeerrichtingen: {
+      Onderhoudstechnologie: {
+        Bachelor:
+          'Professionele Bachelor Elektromechanica - Onderhoudstechnologie',
+        Graduaat:
+          'Graduaat ElektroMechanische Systemen - Onderhoudstechnologie',
+      },
+      Automatisering: {
+        Bachelor: 'Professionele Bachelor Elektromechanica - Automatisering',
+        Graduaat:
+          'Graduaat ElektroMechanische Systemen - Meet- en Regeltechnieken',
+      },
+      Klimatisering: {
+        Bachelor: 'Professionele Bachelor Elektromechanica - Klimatisering',
+        Graduaat: 'Graduaat HVAC-Systemen,',
+      },
+      Elektromechanica: {
+        Bachelor: 'Professionele Bachelor Elektromechanica - Elektromechanica',
+        Graduaat: '',
+      },
+    },
+  },
+  Energietechnologie: {
+    Opleiding: 'Energietechnologie',
+    Afstudeerrichtingen: {
+      Elektrotechniek: {
+        Bachelor: '',
+        Graduaat: '',
+      },
+      'Cleantech-Duurzame Energie': {
+        Bachelor: '',
+        Graduaat: '',
+      },
+    },
+  },
+  'Elektronica-ICT': {
+    Opleiding: 'Elektronica-ICT',
+    Afstudeerrichtingen: {
+      ICT: {
+        Bachelor: 'Professionele Bachelor Elektronica-ICT - ICT',
+        Graduaat: 'Graduaat IoT',
+      },
+      Elektronica: {
+        Bachelor: 'Professionele Bachelor Elektronica-ICT - Elektronica',
+        Graduaat: '',
+      },
+    },
+  },
+  Chemie: {
+    Opleiding: 'Chemie',
+    Afstudeerrichtingen: {
+      Procestechnologie: {
+        Bachelor: '',
+        Graduaat: '',
+      },
+      Milieutechnologie: {
+        Bachelor: '',
+        Graduaat: '',
+      },
+      Chemie: {
+        Bachelor: '',
+        Graduaat: '',
+      },
+      Biochemie: {
+        Bachelor: '',
+        Graduaat: '',
+      },
+    },
+  },
+};
