1bf23d6e5b8629ca351d55bc20f5885738dbeb41
essadji <info@essadji.com>
2021-06-19 23:16:41 +0200

    small change for background

--- james.js
+++ james.js
@@ -1,5 +1,5 @@
 //#region IMPORTS
-import "./opleiding.js";
+import "./fullscreen.js";
 import "./face.js";
 import "./interface.js";
 //#endregion IMPORTS
@@ -8,8 +8,7 @@ import "./interface.js";
 const james_template = document.createElement('template');
 james_template.innerHTML = /* html */ `
 <style>
-    body {margin:0};
-    
+  :host {background: white;}
 </style>
 <fullscreen-ɮ></fullscreen-ɮ>
 <face-ʤ hidden></face-ʤ>
@@ -32,13 +31,20 @@ window.customElements.define('james-ʤ', class extends HTMLElement {
 
   connectedCallback() {
     this.addEventListener("fullscreen", (e) => {
-      let elem = document.documentElement;
-      if (elem.requestFullscreen)
-        elem.requestFullscreen().catch(err => {
-          alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-        });
+      if (this.requestFullscreen)
+      this.requestFullscreen().catch(err => {
+        alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+      });
       if (this.$face.hidden) { this.$face.hidden = false; this.$interface.hidden = true; this.$fullscreen.hidden = true; }
     });
+    // let elem = document.documentElement;
+    // this.addEventListener("fullscreen", (e) => {
+    //   if (elem.requestFullscreen)
+    //     elem.requestFullscreen().catch(err => {
+    //       alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+    //     });
+    //   if (this.$face.hidden) { this.$face.hidden = false; this.$interface.hidden = true; this.$fullscreen.hidden = true; }
+    // });
     this.socket.addEventListener('open', (event) => {
       console.log("opening socket for master component...")
       this.socket.send('Hello server, I\'m a master component; At your service ...');
